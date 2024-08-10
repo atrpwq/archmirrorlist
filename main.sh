bf2f732d6462559f00fbdfbb1f640e09b1bb65d2
drowzy <atrpwq@proton.me>
2024-08-10 16:54:10 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,2 @@
 sudo mv archmirrorlist /etc/pacman.d/
-sudo echo "[extra]" >> /etc/pacman.conf
-sudo echo "Include = /etc/pacman.d/archmirrorlist" >> /etc/pacman.conf
-sudo echo "[multilib]" >> /etc/pacman.conf
-sudo echo "Include = /etc/pacman.d/archmirrorlist" >> /etc/pacman.conf
+sudo echo -e "[extra]\nInclude = /etc/pacman.d/archmirrorlist\n[multilib]\nInclude = /etc/pacman.d/archmirrorlist" >> /etc/pacman.conf
